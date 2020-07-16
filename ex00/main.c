07195bbf458cd6240f1eb9343d62ace57378bd25
lemahlae <lemahlae@student.wethinkcode.co.za>
2020-07-16 12:35:33 +0200

    Colle commit

---
+++ ex00/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemahlae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 15:57:41 by lemahlae          #+#    #+#             */
+/*   Updated: 2020/07/15 17:18:55 by lemahlae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+
+void	ft_putchar(char	c);
+void	rush00(int	x, int	y);
+void	rush01(int	x, int	y);
+void	rush02(int	x, int	y);
+void	rush03(int	x, int	y);
+void	rush04(int	x, int	y);
+
+int		main(void)
+{
+				rush04(5, 5);
+				return	(0);
+}
